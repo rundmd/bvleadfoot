d1d3360b2ec7e019dcf1f41fb5e0bfcb079e4435
rundmd <drew.dimanlig@gmail.com>
2016-07-28 23:13:30 -0500

    Revert "removed sharedFunctions.js"
    This reverts commit 23b6a88416a4cb0b3c95475f0abeb436996f6433.

---
+++ tests/support/pages/common/sharedFunctions.js
@@ -0,0 +1,231 @@
+/**
+ * Created by brendon.kelley on 7/20/16.
+ */
+define([
+  'intern/dojo/node!leadfoot/keys',
+  './pages/elementsPage',
+  './pages/propertiesPage',
+  'require'
+], function(keys, elements, properties,require) {
+  return {
+
+    uploadAFile: function(session, locator, file, type) {
+      if( type=='id') {
+        return this.uploadAFileById(session, locator, file);
+      }
+      else if( type=='xpath') {
+        return this.uploadAFileByXpath(session, locator, file);
+      }
+    },
+
+    clickAButton: function(session, locator, type) {
+      if( type=='xpath' ) {
+        return this.clickAButtonByXapth(session, locator);
+      }
+      else if(type=='id') {
+        return this.clickAButtonById(session, locator);
+      }
+    },
+
+    enterText: function(session, locator, text, type) {
+      if(type=='xpath') {
+        return this.enterTextByXpath(session, locator, text);
+      }
+      else if(type=='id') {
+        return this.enterTextById(session, locator, text);
+      }
+      else if(type=='class') {
+        return this.enterTextByClass(session, locator, text);
+      }
+    },
+
+    enterTextAndEnter: function(session, locator, text, type) {
+      if(type=='xpath') {
+        return this.enterTextAndEnterByXpath(session, locator, text);
+      }
+      else if(type=='id') {
+        return this.enterTextAndEnterById(session, locator, text);
+      }
+      else if(type=='class') {
+        return this.enterTextAndEnterByClass(session, locator, text);
+      }
+    },
+
+    clearTextBox: function(session, locator, num, type) {
+      if(type=='xpath') {
+        return this.clearTextBoxByXpath(session, locator, num);
+      }
+      else if(type=='id') {
+        return this.clearTextBoxById(session, locator, num);
+      }
+    },
+
+    getText: function(session, locator, type) {
+      if(type=='xpath') {
+        return this.getTextByXpath(session, locator);
+      }
+      else if(type=='id') {
+        return this.getTextById(session, locator);
+      }
+    },
+
+
+    enterTextByXpath: function (session, locator, text) {
+      return session
+        .findByXpath(locator)
+        .click()
+        .sleep(2000)
+        .pressKeys(text)
+    },
+
+    enterTextById: function (session, locator, text) {
+      return session
+        .findById(locator)
+        .click()
+        .sleep(2000)
+        .pressKeys(text)
+    },
+
+    enterTextByClass: function (session, locator, text) {
+      return session
+        .findByClassName(locator)
+        .click()
+        .sleep(2000)
+        .pressKeys(text)
+    },
+
+    enterTextAndEnterByXpath: function (session, locator, text) {
+      return session
+        .findByXpath(locator)
+        .click()
+        .sleep(2000)
+        .then( function () {
+          session.pressKeys(text)
+          return session.pressKeys(keys.RETURN)
+        })
+    },
+
+    enterTextAndEnterById: function (session, locator, text) {
+      return session
+        .findById(locator)
+        .click()
+        .sleep(2000)
+        .then( function () {
+          session.pressKeys(text)
+          return session.pressKeys(keys.RETURN)
+        })
+    },
+
+    enterTextAndEnterByClass: function (session, locator, text) {
+      return session
+        .findByClassName(locator)
+        .click()
+        .sleep(2000)
+        .then( function () {
+          session.pressKeys(text)
+          return session.pressKeys(keys.RETURN)
+        })
+    },
+
+    dropDownMenuXpathOptionInput: function (session, locator, option) {
+      return session
+      .findByXpath(locator)
+      .click()
+      .findByXpath(option)
+      .click()
+      .end()
+      .end()
+    },
+
+    dropDownMenuXpathSelectInput: function (session, locator, num) {
+      return session
+      .findByXpath(locator)
+      .click()
+      .end()
+      .then( function () {
+        for( i=0; i<num; i++ ) {
+          session.pressKeys(keys.ARROW_DOWN);
+        }
+        return session.pressKeys(keys.RETURN)
+      })
+    },
+
+    clearTextBoxByXpath: function (session, locator, num) {
+      return session
+      .findByXpath(locator)
+      .click()
+      .then( function () {
+        for(i = 0; i < num; i++){
+          session.pressKeys(keys.BACKSPACE);
+        }
+      })
+    },
+
+    clearTextBoxById: function (session, locator, num) {
+      return session
+      .findById(locator)
+      .click()
+      .then( function () {
+        for(i = 0; i < num; i++){
+          session.pressKeys(keys.BACKSPACE);
+        }
+      })
+    },
+
+    getTextByXpath: function (session, locator) {
+      return session
+      .findByXpath(locator)
+      .getVisibleText()
+    },
+
+    getTextById: function (session, locator) {
+      return session
+      .findById(locator)
+      .sleep(2000)
+      .getVisibleText()
+    },
+
+    getPropertyByXpath: function (session, locator, property) {
+      return session
+      .findByXpath(locator)
+      .sleep(2000)
+      .getProperty(property)
+    },
+
+    clickAButtonByXapth: function(session, locator) {
+      return session
+      .findByXpath(locator)
+      .click()
+      .sleep(1000)
+      .end()
+    },
+
+    clickAButtonById: function(session, locator) {
+      return session
+      .findById(locator)
+      .click()
+      .sleep(1000)
+      .end()
+    },
+
+    uploadAFileById: function(session, id, file) {
+      return session
+      .findById(id)
+      .sleep(2000)
+      .type(require.toUrl(file))
+      .sleep(2000)
+      .end()
+    },
+
+    uploadAFileByXpath: function(session, locator, file) {
+      return session
+      .findByXpath(locator)
+      .sleep(2000)
+      .type(require.toUrl(file))
+      .sleep(2000)
+      .end()
+    }
+
+
+  }
+});
