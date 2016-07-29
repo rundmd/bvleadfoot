c423361c179e639ac73a2f397bee40fc18673917
rundmd <drew.dimanlig@gmail.com>
2016-07-29 00:14:06 -0500

    Revert "add enterTextandEnter wrapper function"
    This reverts commit cf5ab9106c22bbbe2697faf4aea9f69ead8dbdf8.

--- tests/support/pages/common/actionsPage.js
+++ tests/support/pages/common/actionsPage.js
@@ -31,18 +31,6 @@ define([
       }
     },
 
-    enterTextAndEnter: function(session, locator, text, type) {
-      if(type=='xpath') {
-        return this.enterTextAndEnterByXpath(session, locator, text);
-      }
-      else if(type=='id') {
-        return this.enterTextAndEnterById(session, locator, text);
-      }
-      else if(type=='class') {
-        return this.enterTextAndEnterByClass(session, locator, text);
-      }
-    },
-
     clearTextBox: function(session, locator, num, type) {
       if (type == 'xpath') {
         return this.clearTextBoxByXpath(session, locator, num);
